a63bb6c7efc098a09d64a7d66e76fc5740440838
asullyy <karen.silva@icen.ufpa.br>
2021-11-25 18:40:53 -0300

    Update prog04-structs.cpp

--- prog04-structs.cpp
+++ prog04-structs.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
+#include<string>
 
 using std::cout;
 using std::cin;
+using std::string;
 
 //Criar duas structs com cinco campos de tipos diferentes no main e manipular essas structs usando duas funções
 //Criar um array de structs
 //Manipular uma struct usando um ponteiro
 
-int medias();
+void printAlunos (struct aluno cadAlunos[2]);
 
 int main(){
 
     struct indetif{
-        char nome;
-        char escola;
+        string nome;
+        string escola;
         int idade;
         int serie;
         int numMaterias;
@@ -28,50 +30,19 @@ int main(){
     };
 
     struct aluno{
-        struct indetif alunos{
-            char nome;
-            char escola;
-            int idade;
-            int serie;
-            int numMaterias;
-        };
-
-        struct mediasMaterias alunosmat{
-            int portugues;
-            int matematica;
-            int bilogia;
-            int quimica;
-            int fisica;
-        };
+        struct indetif alunos;
+        struct mediasMaterias alunosmat;
     };
 
     struct aluno cadAlunos[2];
 
-    for(int i=0; i=>2; i++){
-        cout<<"Digite o nome do aluno "<<i;
-        cin>> cadAlunos[i].alunos.nome;
-        cout<<"Digite o nome da escola "<<i;
-        cin>> cadAlunos[i].alunos.escola;
-        cout<<"Digite a idade do aluno "<<i;
-        cin>> cadAlunos[i].alunos.idade;
-        cout<<"Digite a serie do aluno "<<i;
-        cin>> cadAlunos[i].alunos.serie;
-        cout<<"Digite o número de matérias "<<i;
-        cin>> cadAlunos[i].alunos.numMaterias;
-    }
-};
 
-int medias(){
-    int notas = 0;
-    int aux = 0;
-
-    for(int i = 0; i<=4; i++){
-        cout<< "Digite uma nota: ";
-        cin>>aux;
-        notas = notas + aux;
-    };
-
-    int media = notas/4;
+    cout << "Digite o nome do aluno 0: ";
+    cin >> cadAlunos[0].alunos.nome;
     
-    return media;
 }
+
+void printAlunos(struct aluno cadAlunos[]) {
+    cout << cadAlunos[0].alunos.nome;
+}
+
